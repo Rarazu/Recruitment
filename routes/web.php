c24ee68b3fd92c7842143d46159e7c39c9d79ee7
zaky <ilhamzaky.d@corp.bm.co.id>
2022-06-02 18:00:55 +0700

    user form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * Membuka Halaman Form User
+ */
+Route::get('/user', [UserController::class, 'index']);
+
+/**
+ * Submit Form User
+ */
+Route::post('/user/store', [UserController::class, 'store']);
