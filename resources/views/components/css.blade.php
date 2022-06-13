b255d62f5bd8ab0eb550d36b92680a6654f204f6
zaky <ilhamzaky.d@corp.bm.co.id>
2022-06-13 10:38:57 +0700

    layouting

---
+++ resources/views/components/css.blade.php
@@ -0,0 +1,7 @@
+<link href="{{ asset('coreui-v2/vendors/@coreui/icons/css/coreui-icons.min.css') }}" rel="stylesheet">
+<link href="{{ asset('coreui-v2/vendors/flag-icon-css/css/flag-icon.min.css') }}" rel="stylesheet">
+<link href="{{ asset('coreui-v2/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+<link href="{{ asset('coreui-v2/vendors/simple-line-icons/css/simple-line-icons.css') }}" rel="stylesheet">
+<!-- Main styles for this application-->
+<link href="{{ asset('coreui-v2/css/style.css') }}" rel="stylesheet">
+<link href="{{ asset('coreui-v2/vendors/pace-progress/css/pace.min.css') }}" rel="stylesheet">
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!--
+* CoreUI - Free Bootstrap Admin Template
+* @version v2.1.15
+* @link https://coreui.io
+* Copyright (c) 2018 creativeLabs Łukasz Holeczek
+* Licensed under MIT (https://coreui.io/license)
+-->
+
+<html lang="en">
+
+<head>
+    <base href="./">
+    @include('components.meta')
+    <title>CoreUI Free Bootstrap Admin Template</title>
+    <!-- Icons-->
+    @include('components.css')
+    <!-- Global site tag (gtag.js) - Google Analytics-->
+    <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-118965717-3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        // Shared ID
+        gtag('config', 'UA-118965717-3');
+        // Bootstrap ID
+        gtag('config', 'UA-118965717-5');
+
+    </script>
+</head>
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+    @include('components.header')
+    <div class="app-body">
+        @include('components.sidebar')
+        <main class="main">
+            @include('components.breadcrumbs')
+            <div class="container-fluid">
+                <div class="animated fadeIn">
+                    HALO
+                </div>
+            </div>
+        </main>
+    </div>
+    <footer class="app-footer">
+        <div>
+            <a href="https://coreui.io">CoreUI</a>
+            <span>&copy; 2018 creativeLabs.</span>
+        </div>
+        <div class="ml-auto">
+            <span>Powered by</span>
+            <a href="https://coreui.io">CoreUI</a>
+        </div>
+    </footer>
+    <!-- CoreUI and necessary plugins-->
+    <script src="{{ asset('coreui-v2/vendors/jquery/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('coreui-v2/vendors/popper.js/js/popper.min.js') }}"></script>
+    <script src="{{ asset('coreui-v2/vendors/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('coreui-v2/vendors/pace-progress/js/pace.min.js') }}"></script>
+    <script src="{{ asset('coreui-v2/vendors/perfect-scrollbar/js/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('coreui-v2/vendors/@coreui/coreui/js/coreui.min.js') }}"></script>
+</body>
+
+</html>
